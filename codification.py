0368a2620f87fdd26500cdf0787c1f449f62c5b5
Álvaro Ramajo <47662166+ramajoballester@users.noreply.github.com>
2021-04-22 19:02:45 +0200

    Update codification.py

--- codification.py
+++ codification.py
@@ -165,93 +165,97 @@ empty_cell = sheet[chr(ord(cell_letter)+2) + cell_number].value
 
 while verb and not empty_cell:
     if infinitive:
-        conjugation = cg.conjugate(str(infinitive))
-        conj = conjugation['moods']
-        conj = remove_suj(conj)
+        try:
+            conjugation = cg.conjugate(str(infinitive))
+            conj = conjugation['moods']
+            conj = remove_suj(conj)
 
-        modes_match, verb_times_match, pers_match = get_verb_time(conj, verb)
-        infinitive = conj['infinitivo']['infinitivo'][0]
+            modes_match, verb_times_match, pers_match = get_verb_time(conj, verb)
+            infinitive = conj['infinitivo']['infinitivo'][0]
 
-        Gcell = None
-        for each in pers_match:
-            if type(Gcell) == type(None):
-                Gcell = each
-            elif Gcell != each:
-                Gcell = str(Gcell) + '/' + str(each)
+            Gcell = None
+            for each in pers_match:
+                if type(Gcell) == type(None):
+                    Gcell = each
+                elif Gcell != each:
+                    Gcell = str(Gcell) + '/' + str(each)
 
-        Kcell = None
-        for each in modes_match:
-            if type(Kcell) == type(None):
-                Kcell = getKcell(each)
-            elif Kcell != getKcell(each):
-                Kcell = str(Kcell) + '/' + str(getKcell(each))
+            Kcell = None
+            for each in modes_match:
+                if type(Kcell) == type(None):
+                    Kcell = getKcell(each)
+                elif Kcell != getKcell(each):
+                    Kcell = str(Kcell) + '/' + str(getKcell(each))
 
-        Lcell = None
-        for each_verb_time, each_mode in zip(verb_times_match, modes_match):
-            if type(Lcell) == type(None):
-                Lcell = getLcell(each_mode, each_verb_time)
-            elif Lcell != getLcell(each_mode, each_verb_time):
-                Lcell = str(Lcell) + '/' + str(getLcell(each_mode, each_verb_time))
+            Lcell = None
+            for each_verb_time, each_mode in zip(verb_times_match, modes_match):
+                if type(Lcell) == type(None):
+                    Lcell = getLcell(each_mode, each_verb_time)
+                elif Lcell != getLcell(each_mode, each_verb_time):
+                    Lcell = str(Lcell) + '/' + str(getLcell(each_mode, each_verb_time))
 
-        Mcell = None
-        for each in verb_times_match:
-            if type(Mcell) == type(None):
-                Mcell = getMcell(each)
-            elif Mcell != getMcell(each):
-                Mcell = str(Mcell) + '/' + str(getMcell(each))
+            Mcell = None
+            for each in verb_times_match:
+                if type(Mcell) == type(None):
+                    Mcell = getMcell(each)
+                elif Mcell != getMcell(each):
+                    Mcell = str(Mcell) + '/' + str(getMcell(each))
 
-        Ncell = None
-        for each in verb_times_match:
-            if type(Ncell) == type(None):
-                Ncell = getNcell(each)
-            elif Ncell != getNcell(each):
-                Ncell = str(Ncell) + '/' + str(getNcell(each))
+            Ncell = None
+            for each in verb_times_match:
+                if type(Ncell) == type(None):
+                    Ncell = getNcell(each)
+                elif Ncell != getNcell(each):
+                    Ncell = str(Ncell) + '/' + str(getNcell(each))
 
-        if len(modes_match) > 1:
-            Ocell = 1
-        else:
-            Ocell = 0
+            if len(modes_match) > 1:
+                Ocell = 1
+            else:
+                Ocell = 0
 
-#         if infinitive in P0:
-#             Pcell = 0
-#         elif infinitive in P1:
-#             Pcell = 1
-#         elif infinitive in P2:
-#             Pcell = 2
-#         elif infinitive in P3:
-#             Pcell = 3
-#         elif infinitive in P4:
-#             Pcell = 4
-#         elif infinitive in P5:
-#             Pcell = 5
-#         elif infinitive in P6:
-#             Pcell = 6
-#         else:
-#             Pcell = -1
-
-
-        sheet[chr(ord(cell_letter)+3) + cell_number].value = str(Gcell)
-        sheet[chr(ord(cell_letter)+7) + cell_number].value = str(Kcell)
-        sheet[chr(ord(cell_letter)+8) + cell_number].value = str(Lcell)
-        sheet[chr(ord(cell_letter)+9) + cell_number].value = str(Mcell)
-        sheet[chr(ord(cell_letter)+10) + cell_number].value = str(Ncell)
-        sheet[chr(ord(cell_letter)+11) + cell_number].value = str(Ocell)
-#         sheet[chr(ord(cell_letter)+12) + cell_number].value = str(Pcell)
-
-        if args.color:
-            sheet[chr(ord(cell_letter)+3) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+7) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+8) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+9) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+10) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+11) + cell_number].fill = fill
-            sheet[chr(ord(cell_letter)+12) + cell_number].fill = fill
+    #         if infinitive in P0:
+    #             Pcell = 0
+    #         elif infinitive in P1:
+    #             Pcell = 1
+    #         elif infinitive in P2:
+    #             Pcell = 2
+    #         elif infinitive in P3:
+    #             Pcell = 3
+    #         elif infinitive in P4:
+    #             Pcell = 4
+    #         elif infinitive in P5:
+    #             Pcell = 5
+    #         elif infinitive in P6:
+    #             Pcell = 6
+    #         else:
+    #             Pcell = -1
 
 
+            sheet[chr(ord(cell_letter)+3) + cell_number].value = str(Gcell)
+            sheet[chr(ord(cell_letter)+7) + cell_number].value = str(Kcell)
+            sheet[chr(ord(cell_letter)+8) + cell_number].value = str(Lcell)
+            sheet[chr(ord(cell_letter)+9) + cell_number].value = str(Mcell)
+            sheet[chr(ord(cell_letter)+10) + cell_number].value = str(Ncell)
+            sheet[chr(ord(cell_letter)+11) + cell_number].value = str(Ocell)
+    #         sheet[chr(ord(cell_letter)+12) + cell_number].value = str(Pcell)
 
-        print(' ')
-        print(verb)
-        print(infinitive)
+            if args.color:
+                sheet[chr(ord(cell_letter)+3) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+7) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+8) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+9) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+10) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+11) + cell_number].fill = fill
+                sheet[chr(ord(cell_letter)+12) + cell_number].fill = fill
+
+
+
+            print(' ')
+            print(verb)
+            print(infinitive)
+        
+        except:
+            pass
 
     cell_number = str(int(cell_number)+1)
     verb = sheet[cell_letter + cell_number].value
