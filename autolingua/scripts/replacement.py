e587f5ee792807a528766a213851db841edcd7e8
ramajoballester <alvaroramajo1@gmail.com>
2024-07-21 11:05:18 +0200

    Add warning for keys - values length mismatch

--- autolingua/scripts/replacement.py
+++ autolingua/scripts/replacement.py
@@ -1,6 +1,7 @@
 import openpyxl
 import argparse
 import os
+import sys
 import shutil
 
 parser = argparse.ArgumentParser(description='Automated replacement program')
@@ -19,8 +20,19 @@ parser.add_argument('-fase3_from_file', default='None', help='Extract Fase 3 dat
 args = parser.parse_args()
 
 
-def get_column_data(sheet, column):
-    return [cell.value for cell in sheet[column] if cell.value is not None]
+def get_column_data(sheet, column, return_none = False):
+    if return_none:
+        return [cell.value for cell in sheet[column]]
+    else:
+        return [cell.value for cell in sheet[column] if cell.value is not None]
+    
+def get_dict(sheet, column1, column2, info_fase = 2):
+    keys = get_column_data(sheet, column1)[1:]
+    values = get_column_data(sheet, column2)[1:]
+    # If length of keys and values is different, return None
+    if len(keys) != len(values):
+        raise ValueError(f'Length of keys and values is different in Fase {info_fase}: {len(keys)} vs {len(values)}')
+    return dict(zip(keys, values))
 
 fases_filename = os.path.join('..', args.fases_filename + args.extension)
 fases_workbook = openpyxl.load_workbook(filename=fases_filename)
@@ -28,16 +40,13 @@ fases_sheet = fases_workbook.worksheets[0]
 
 
 # Load fase_n changes from columns
-f2_1 = dict(zip(get_column_data(fases_sheet, args.fase2_1_col[0])[1:],
-                get_column_data(fases_sheet, args.fase2_1_col[1])[1:]))
-f2_2 = dict(zip(get_column_data(fases_sheet, args.fase2_2_col[0])[1:],
-                get_column_data(fases_sheet, args.fase2_2_col[1])[1:]))
-f2_3 = dict(zip(get_column_data(fases_sheet, args.fase2_3_col[0])[1:],
-                get_column_data(fases_sheet, args.fase2_3_col[1])[1:]))
-f3 = dict(zip(get_column_data(fases_sheet, args.fase3_col[0])[1:],
-                get_column_data(fases_sheet, args.fase3_col[1])[1:]))
-f4 = dict(zip(get_column_data(fases_sheet, args.fase4_col[0])[1:],
-                get_column_data(fases_sheet, args.fase4_col[1])[1:]))
+keys = get_column_data(fases_sheet, args.fase2_1_col[0])[1:]
+values = get_column_data(fases_sheet, args.fase2_1_col[1])[1:]
+f2_1 = get_dict(fases_sheet, args.fase2_1_col[0], args.fase2_1_col[1], info_fase='fase 2.1')
+f2_2 = get_dict(fases_sheet, args.fase2_2_col[0], args.fase2_2_col[1], info_fase='fase 2.2')
+f2_3 = get_dict(fases_sheet, args.fase2_3_col[0], args.fase2_3_col[1], info_fase='fase 2.3')
+f3 = get_dict(fases_sheet, args.fase3_col[0], args.fase3_col[1], info_fase='fase 3')
+f4 = get_dict(fases_sheet, args.fase4_col[0], args.fase4_col[1], info_fase='fase 4')
 
 f2 = f2_1.copy()
 f2.update(f2_2)
@@ -65,21 +74,32 @@ for i in range(len(from_file)):
         output_workbook = openpyxl.load_workbook(filename=output_filename)
         output_sheet = output_workbook.worksheets[0]
 
-        input_rows = get_column_data(input_sheet, args.input_col[0])[1:]
+        input_rows = get_column_data(input_sheet, args.input_col[0], return_none=True)[1:]
 
         for each_row in range(len(input_rows)):
-            new_row = [each[1:] if each[0] == ' ' else each for each in input_rows[each_row].split(',')]
-            n_words[i] += len(new_row)
-            for each_key in f_dict[i].keys():
-                for each_word in range(len(new_row)):
-                    if new_row[each_word] == each_key:
-                        f_dict_changes[i] += 1
-                        new_row[each_word] = f_dict[i][each_key]
+            # print(f'Input row: {each_row}: {input_rows[each_row]}')
+            if input_rows[each_row] is not None:
+                new_row = [each[1:] if each[0] == ' ' else each for each in input_rows[each_row].split(',')]
+                # print(f'After split: {new_row}')
+                
+                n_words[i] += len(new_row)
+                for each_key in f_dict[i].keys():
+                    for each_word in range(len(new_row)):
+                        if new_row[each_word] == each_key:
+                            f_dict_changes[i] += 1
+                            # print(f'Replacing {new_row[each_word]} with {f_dict[i][each_key]}')
+                            new_row[each_word] = f_dict[i][each_key]
+                # print(f'After replacement: {new_row}')
 
-            try:
-                new_row = ', '.join(new_row)
-            except:
-                print(new_row)
+                try:
+                    new_row = ', '.join(new_row)
+                except:
+                    print(new_row)
+            
+            else:
+                new_row = ''
+                print(f'Empty row: {each_row}: {new_row}')
+            # print(f'Output row {each_row}: {new_row}\n')
             output_sheet[args.input_col + str(each_row + 2)].value = new_row
         output_workbook.save(filename=output_filename)
 
